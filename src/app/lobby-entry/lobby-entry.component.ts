6b4432b5a9d238a20520480cb050a4fa747e0bb6
Enrico Schwendig <enrico.schw@gmx.de>
2023-12-21 12:53:36 +0100

    add shig sdk lobby in web client

--- src/app/lobby-entry/lobby-entry.component.ts
+++ src/app/lobby-entry/lobby-entry.component.ts
@@ -12,26 +12,26 @@ type Param = { streamId: string, spaceId: string, userToken: string }
   styleUrls: ['./lobby-entry.component.scss']
 })
 export class LobbyEntryComponent implements OnInit {
-  // // params: Observable<Param> = new BehaviorSubject(null)
-  //
-  // streamId: string;
-  // spaceId: string;
-  // userToken: string;
-  // role$: Observable<string>;
-  //
-  // constructor(
-  //   private route: ActivatedRoute,
-  //   session: SessionService,
-  // ) {
-  //   this.userToken = session.getToken();
-  //   this.role$ = session.getUserName().pipe(map((name: string) => name === "Owner"? "owner" : "guest"));
-  //   const streamId = this.route.snapshot.paramMap.get('streamId');
-  //   const spaceId = this.route.snapshot.paramMap.get('spaceId');
-  //
-  //   this.streamId = streamId ? streamId : '';
-  //   this.spaceId = spaceId ? spaceId : '';
-  // }
-  //
+  // params: Observable<Param> = new BehaviorSubject(null)
+
+  streamId: string;
+  spaceId: string;
+  userToken: string;
+  role$: Observable<string>;
+
+  constructor(
+    private route: ActivatedRoute,
+    session: SessionService,
+  ) {
+    this.userToken = session.getToken();
+    this.role$ = session.getUserName().pipe(map((name: string) => name === "Owner"? "owner" : "guest"));
+    const streamId = this.route.snapshot.paramMap.get('streamId');
+    const spaceId = this.route.snapshot.paramMap.get('spaceId');
+
+    this.streamId = streamId ? streamId : '';
+    this.spaceId = spaceId ? spaceId : '';
+  }
+
   ngOnInit(): void {
 
     // this.streamId = this.route.snapshot.paramMap.get('streamId');
